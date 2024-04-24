b6970e27dc288857375bed42f7608f967fddb2e1
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-04-24 15:31:46 +0100

    fixing task-2 & task-3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,15 @@
-import { uploadPhoto, createUser } from './utils';
+/* eslint-disable no-console */
+import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(
-        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`,
-      );
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => {
-      console.log('Signup system offline');
+      console.log("Signup system offline");
     });
 }
