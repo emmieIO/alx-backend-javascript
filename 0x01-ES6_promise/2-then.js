1377c0b158ed49f27f8c25f5ac6bd6f8d1d3d2f6
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-04-24 15:29:35 +0100

    fixing task-2 & task-3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-var */
 export default function handleResponseFromAPI(promise) {
-  promise.then((response) => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'success',
-    };
-  })
-    .catch((err) => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    console.log(
-      `${values[0].body} ${values[1].firstName} ${values[1].lastName} `,
-    );
-  });
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(
+        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`,
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
