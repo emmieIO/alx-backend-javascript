ff4c2b3256709eba2b1965854b88b48b5af56a77
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-04-25 13:09:33 +0100

    commit for task0 - task2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Type checking and error handling
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (
+      !Array.isArray(students)
+      || students.some((student) => typeof student !== 'string')
+    ) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    // Store attributes with underscore prefix
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    // Return a copy of the students array to prevent modification from outside
+    return this._students.slice();
+  }
+
+  // Setters
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+}
