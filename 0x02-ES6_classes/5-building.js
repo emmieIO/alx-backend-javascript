691a7558b6be91258559bd6b19fffebbbd7a33d5
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-04-25 16:19:49 +0100

    commit for task0 - task

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,16 +3,14 @@ class Building {
     this._sqft = sqft;
     if (new.target !== Building && !this.evacuationWarningMessage) {
       throw new Error(
-        "Class extending Building must override evacuationWarningMessage"
+        'Class extending Building must override evacuationWarningMessage',
       );
     }
   }
 
   get sqft() {
     return this._sqft;
-    
   }
-
 }
 
 export default Building;
--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -1,10 +1,10 @@
-import Building from "./5-building.js";
+import Building from './5-building.js';
 
 const b = new Building(100);
 console.log(b);
 
 class TestBuilding extends Building {
-    
+
 }
 
 try {
