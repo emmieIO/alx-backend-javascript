3fc6a099dd6aaa2935a4b095249914716b39e289
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-04-30 06:30:47 +0100

    pushign 8,9,10

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return '';
-  }
-  const setArray = [...set];
-  return setArray.filter((str) => str.startsWith(startString))
-    .map((str) => str.substr(startString.length))
-    .join('-');
+  const filteredValues = [...set].filter((str) => str.startsWith(startString));
+  const cleanedValues = filteredValues.map((str) => str.substr(startString.length));
+  return cleanedValues.join('-');
 }
