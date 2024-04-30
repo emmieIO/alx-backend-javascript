cf2a1cbd240f7e2ecb7951cad3d4a2c40f5f0e6f
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-04-30 04:50:37 +0100

    task for filter correction

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,5 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-  const studentsByCity = getStudentsByLocation(getListStudents, city);
+  const studentsByCity = getListStudents.filter((std) => std.location === city);
   const updatedGrades = studentsByCity.map((std) => {
     const graded = newGrades.find((grade) => (std.id === grade.studentId));
     const updatedStd = { ...std };
