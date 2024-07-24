9868d1960087eda9973c5546b78bc788503373e1
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-07-24 03:23:41 +0100

    0 - 7 fix

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
-module.exports = function displayMessage(msg){
-    console.log(msg);
+module.exports = function displayMessage(msg) {
+  console.log(msg);
+};
+function displayMessage(message) {
+  console.log(message);
 }
+
+module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,16 @@
-// 1-stdin.js
-const readline = require("readline");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-// Create an interface for reading input from stdin and writing output to stdout
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Ask the user for their name
-rl.question("", (answer) => {
-  // Display the user's name
-  console.log(`Your name is: ${answer}`);
-
-  // Close the readline interface
-  rl.close();
-});
-
-// Listen for the close event to display the closing message
-rl.on("close", () => {
-  console.log("This important software is now closing");
-});
+if (process.stdin.isTTY) {
+  process.stdin.on("data", (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on("data", (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on("exit", () => {
+    process.stdout.write("This important software is now closing\n");
+  });
+}
