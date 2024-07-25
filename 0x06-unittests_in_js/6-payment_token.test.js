bfd7a4ebe3129e3096d2f1f114508dac6b8bacef
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-07-25 17:40:40 +0100

    6-payment

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+const expect = chai.expect;
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a resolved promise with the correct object when success is true', function(done) {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    }).catch((error) => {
+      done(error);
+    });
+  });
+});
