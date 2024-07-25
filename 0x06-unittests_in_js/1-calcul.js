aa982106fae708ab3b12bd9abb81cd518698f05d
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-07-25 06:23:25 +0100

    1-calcul.js

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,20 @@
+function calculateNumber(type, a, b) {
+  let num1 = Math.round(a);
+  let num2 = Math.round(b);
+  
+  if (type == "SUM") {
+    return num1 + num2;
+  }
+  if (type == "SUBTRACT") {
+    return num1 - num2;
+  }
+
+  if (type == "DIVIDE") {
+    if (num2 == 0) {
+      return "Error";
+    }
+    return num1 / num2;
+  }
+}
+
+module.exports = calculateNumber;
